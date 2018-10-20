3a3235ef8c69a57579d182b69119fa99d5155f8e
feng <1614404677@qq.com>
2018-10-20 13:10:31 +0800

    urls

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def fengguifan():
 @app.route('/greet',defaults={'name':'fengguifan'})
 @app.route('/greet/<name>')
 def greet(name):
-    return "<h1>Hello,%s!</h1> % name"
+    return "<h1>Hello,%s!</h1>" % name
