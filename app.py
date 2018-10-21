ab9c8bbb3fbdedd3a4beafb45469942dac70ae06
feng <1614404677@qq.com>
2018-10-21 20:17:16 +0800

    urlplus

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+from flask import request
 from flask import Flask
 
 app = Flask(__name__)
@@ -19,3 +20,16 @@ def fengguifan():
 @app.route('/greet/<name>')
 def greet(name):
     return "<h1>Hello,%s!</h1>" % name
+
+#获取查询字符串
+#使用示例/hello?name=feng
+#需要注意的是，这里存在安全漏洞
+@app.route('/hello',methods=['GET','POST'])
+def hello():
+    name = request.args.get('name','Flask')
+    return "<h1>hello,%s!</h1>"  % name
+
+#url变量转换器
+@app.route('/goto/<int:year>')
+def goto(year):
+    return '<h1>Welcome to %s!<h1>' % year
