d193c8408497c2ce03fb7bd311b4808a04a7303c
feng <1614404677@qq.com>
2018-11-16 22:18:39 +0800

    about templates

--- app.py
+++ app.py
@@ -1,57 +1,30 @@
-# -*- coding:utf-8 -*-
-from flask import request,make_response,url_for
-from flask import Flask,redirect
+from flask import Flask, redirect,request, make_response, url_for, render_template
+
 
 app = Flask(__name__)
 
+user = {
+    'username': 'feng',
+    'bio': 'this guy are lazy,nothing left',
+}
+books = [{'name': 'computer', 'year': 1998},
+         {'name': 'caozuoxitong', 'year': 2008},
+         {'name': 'jisuanjiwangluo', 'year': 2010},]
+
+
 @app.route('/')
-def index():
-    return "<h1>Hello,is me</h1>"
+def showbooks():
+    return render_template('books.html', books=books, user=user)
 
-#绑定多个url
-@app.route('/fengguifan')
-@app.route('/fengxuezhang')
-def fengguifan():
-    return "<h1>Hello,i am fengguifan</h1>"
+@app.route('/base')
+def basetmp():
+    return render_template('base.html')
 
-#动态url
-#为设置默认参数
-@app.route('/greet',defaults={'name':'fengguifan'})
-@app.route('/greet/<name>')
-def greet(name):
-    return "<h1>Hello,%s!</h1>" % name
+@app.route('/child')
+def childtmp():
+    return render_template('child.html')
 
-#获取查询字符串
-#使用示例/hello?name=feng
-#需要注意的是，这里存在安全漏洞
-@app.route('/hello',methods=['GET','POST'])
-def hello():
-    name = request.args.get('name','Flask')
-    return "<h1>hello,%s!</h1>"  % name
-
-#url变量转换器
-@app.route('/goto/<int:year>')
-def goto(year):
-    return '<h1>Welcome to %s!<h1>' % year
-
-@app.route('/colors/<any(blue,white,red):color>')
-def three_colors(color):
-    return '<h1>color %s </h1> ' %color
-#Hook
-'''
-@app.before_request
-def do_something():
-    return '<h1>hahahhahaha</h1>'
-'''
-@app.route('/set/<name>')
-def set_cookie(name):
-    response = make_response(redirect(url_for('hi')))
-    response.set_cookie('name',name)
-    return response
-
-@app.route('/hi')
-def hi():
-    name = request.args.get('name')
-    if name is None:
-        name = request.cookies.get('name','Human')
-    return '<h1>Hello,%s</h1>' %name
+def inject_feng():
+    feng = {'name':'feng',}
+    return dict(feng=feng)
+app.context_processor(inject_feng)
