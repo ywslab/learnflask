9842d6fe1b8d6b56c6bcbc7f10fc9f5fef82da4b
feng <1614404677@qq.com>
2018-11-17 16:18:47 +0800

    db part1

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from flask import Flask, redirect,request, make_response, url_for, render_template
 from learnflask.forms import LoginForm
-
-
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+import os
+import click
 app = Flask(__name__)
-
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+bootstrap.init_app(app)
+db.init_app(app)
+from learnflask.models import User
 app.secret_key=('fdasdfasd')
-user = {
-    'username': 'feng',
-    'bio': 'this guy are lazy,nothing left',
-}
-books = [{'name': 'computer', 'year': 1998},
-         {'name': 'caozuoxitong', 'year': 2008},
-         {'name': 'jisuanjiwangluo', 'year': 2010},]
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL','sqlite:///' + os.path.join(app.root_path,'data.db'))
 
 @app.route('/')
 def showbooks():
-    return render_template('books.html', books=books, user=user)
+    return render_template('books.html')
 
 @app.route('/base')
 def basetmp():
@@ -37,3 +36,12 @@ def formtest():
     if form.validate_on_submit():
         print(form.username.data)
     return render_template('form.html',form=form)
+
+@app.cli.command()
+def initdb():
+    db.drop_all()
+    db.create_all()
+    click.echo(' Initialized database.')
+    #可以在命令行下输入 flask initdb即可创建数据库和表
+
+
